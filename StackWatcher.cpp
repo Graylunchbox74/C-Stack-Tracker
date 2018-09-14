e5d971bf044a679ff2e3005d3b8122b26f699f8a
Graylunchbox74 <collin.rumpca@trojans.dsu.edu>
2018-09-14 15:25:40 -0500

    now gives output in correct format

--- StackWatcher.cpp
+++ StackWatcher.cpp
@@ -6,16 +6,16 @@
 #include <tuple>
 #include <stack>
 #include <queue>
+#include <sstream>
 
 using namespace std;
 
-//global variables - yes, I do need them to be global
+//global variables
 queue<int> inputQueue;
+int currentX, currentY;
+string currentStack;
 
 
-
-
-// //print tuple
 template<class Tuple, size_t N>
 struct TuplePrinter {
     static void print(const Tuple& t) 
@@ -57,9 +57,9 @@ tuple<Tfirst, Trest...> createTupleObject(Tfirst first, Trest... rest){
 template <typename... Tlist>
 stack<tuple<Tlist...>> createStack(Tlist... list){
     auto tmp = createTupleObject(list...);
-    auto x = tuple_size<decltype(tmp)>::value;
-    stack<decltype(tmp)> stackOfTuples;
+//    auto x = tuple_size<decltype(tmp)>::value;
 
+    stack<decltype(tmp)> stackOfTuples;
     stackOfTuples.push(tmp);
     return stackOfTuples;
 }
@@ -90,11 +90,86 @@ int setup(){
     init_pair(7, COLOR_BLACK, COLOR_CYAN);
 }
 
+void printMenu(){
+    stringstream ss;
+    auto old_buf = cout.rdbuf(ss.rdbuf());
+    setup();
+    int maxX, maxY;
+
+    getmaxyx(stdscr,maxY,maxX);
+
+    attron(COLOR_PAIR(4));
+        mvprintw(0,0,"Program Output:");
+        mvprintw(0,maxX/2+2,"StackTracker:");
+    attroff(COLOR_PAIR(4));
+
+    attron(COLOR_PAIR(1));
+        for(int i = 0; i < maxX; i++){
+            mvprintw(2,i,"-");
+        }
+
+        for(int i = 0; i < maxY; i++){
+            mvprintw(i,maxX/2,"|");
+        }
+
+        mvprintw(2,maxX/2,"+");
+    attroff(COLOR_PAIR(1));
+
+    currentX = maxX / 2;
+    currentY = maxY - (maxY/10);
+
+    auto testStack = createStack(1,2,"hello world");
+    testStack.push(createTupleObject(1,2,"test"));
+    testStack.push(createTupleObject(1,2,"test"));
+    testStack.push(createTupleObject(1,2,"test"));
+    testStack.push(createTupleObject(1,2,"testiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiin"));
+
+
+    while(!testStack.empty()){
+        for(int i = maxX/2 + 1; i < maxX; i++){
+            mvprintw(currentY,i,"-");
+        }
+        print(testStack.top());
+
+
+        queue<string> eachLine;
+        int nextStart = 0;
+        while((ss.str().size() - nextStart) > maxX/2 - 1){
+            eachLine.push(ss.str().substr(0,maxX/2 - 1));
+            nextStart += maxX/2;
+        }
+        eachLine.push(ss.str().substr(nextStart,ss.str().size()));
+   
+        for(int i = 1; !eachLine.empty() && i <= (maxY/10); i++) {
+            mvprintw(currentY+i,maxX/2+1,"%s",eachLine.front().c_str());
+            eachLine.pop();
+        }
+   
+        ss.str("");
+
+        testStack.pop();
+        currentY = currentY - (maxY/10);
+    }
+
+    cout.rdbuf(old_buf);
+
+    while(inputQueue.empty()){ input(); }
+
+    return;    
+}
+
 int main(int argc, char ** argv){
-    int x = 0;
-    int y = 2;
-    string z = "hello world";
-    auto p = createStack(x,y,z);
-    print(p.top());
+
+    printMenu();
+    // int x = 0;
+    // int y = 2;
+    // string z = "hello world";
+    // auto p = createStack(x,y,z);
+    // p.push(createTupleObject(x,y,z));
+    // while(!p.empty()){
+    //     print(p.top());
+    //     p.pop();
+    // }
+   endwin();
     return 0;
 }
