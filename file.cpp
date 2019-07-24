0290f11f7ef7c35cd05f1c63f998b74f9120d9e9
Trevor Bautista <trevorbautista@gmail.com>
2019-07-24 13:28:59 -0600

    Major changes:  - Improved function finder algorithm (finding end of specified function)  - Fixed other minor errors

--- file.cpp
+++ file.cpp
@@ -1,15 +1,16 @@
 #include <vector>
 #include <iostream>
+#include <string>
 
-int factorial(int n, char c){  
+int factorial(int n, char c, int l, std::string z){  
     if(n == 1){
         return 1;
     }
 
-    int p = n * factorial(n-1,c+1);
+    int p = n * factorial(n-1,c+1,2,z);
     return p;
 }
 
 int main() {
-    factorial(10,'a');
+    factorial(10,'a',2, "hello!!!!!!!!");
 }
